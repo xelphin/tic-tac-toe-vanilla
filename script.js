5f3a52b10beed9dfc8525616b38d1060d3cfc1eb
xelphin <111276630+xelphin@users.noreply.github.com>
2023-11-15 21:11:45 +0200

    Rearrange File

--- script.js
+++ script.js
@@ -6,40 +6,119 @@ const lineSize = 3;
 const emptyString = " ";
 
 // --------------------------------------------
-//                  HELPERS
+// --------------------------------------------
+//                   MAIN
+// --------------------------------------------
 // --------------------------------------------
 
-const HelperDOM = (function () {
-    const findNodeIndex = (node, nodeList) => {
-        for (let i = 0; i < nodeList.length; i++) {
-            if (nodeList[i] === node) {
-                return i;
-            }
+
+const Game = (function () {
+    
+    const player1 = createPlayer(1, "player1", "X");
+    const player2 = createPlayer(2, "player2", "O");
+    let currPlayer = player1;
+    let gameEnded = false;
+    let gameState = -1;
+
+    const reset = () => {
+        currPlayer = player1;
+        gameEnded = false;
+        Board.reset();
+        gameState = -1;
+        Game_DOM.resetStatus();
+        console.log("Reset Game");
+    }
+
+    const playTurn_aux = (index) => {
+        if (gameEnded) {
+            console.log("Game has already ended.");
+            return currPlayer.getNum();
         }
+        if (!Board.addSignToBlock(currPlayer, index)) {
+            return -2;
+        }
+
+        if (Board.checkWin()) {
+            currPlayer.incScore();
+            return currPlayer.getNum();
+        }
+        if (Board.checkFull()) {
+            return 0;
+        }
+
+        currPlayer == player1 ? currPlayer = player2 : currPlayer = player1;
         return -1;
     }
 
-    return { findNodeIndex };
-})();
-
-// --------------------------------------------
-//                   PLAYER
-// --------------------------------------------
-
-const Players_DOM = (function () {
-    
-    const playerScores = document.querySelectorAll(".player-score");
-
-    const incScore = (playerNum, score) => {
-        let index = playerNum -1;
-        if (index < 0 || index > playerScores.length) return;
-        playerScores[index].textContent = "Score: "+score.toString(10);
+    const playTurn = (index) => {
+        gameState = playTurn_aux(index);
+        if (gameState >=0 ) {
+            gameEnded = true;
+            Game_DOM.showStartBtn();
+            Game_DOM.showStatus();
+        }
     }
 
-    return {incScore };
+    const getGameState = () => gameState;
+
+    const start = () => {
+        Game_DOM.setStartFunctionality();
+    }
+
+    return {reset, playTurn, getGameState, start};
 })();
 
+const Game_DOM = (function () {
+    
+    const statusInfo = document.querySelector("#status-info");
+    const startBtn = document.querySelector("#start-btn");
 
+    const hideStartBtn = () => {
+        startBtn.style.display = 'none';
+        startBtn.textContent = "Play Again";
+    }
+
+    const showStartBtn = () => {
+        startBtn.style.display = 'block';
+    }
+
+    const showStatus = () => {
+        let gameState = Game.getGameState();
+        if (gameState == -1 || gameState == -2) {
+            return;
+        }
+        if (gameState == 0) {
+            statusInfo.textContent = "Tie"; 
+        } else {
+            statusInfo.textContent = "Player"+gameState+" Won!"; 
+        }
+    }
+
+    const resetStatus = () => {
+        statusInfo.textContent = "";
+    }
+
+    const setStartFunctionality = () => {
+        startBtn.addEventListener("click", () => {
+            Game.reset();
+            hideStartBtn();
+            Board.allowPlaying();
+        });
+    }
+
+    return { hideStartBtn, showStartBtn, showStatus, resetStatus, setStartFunctionality };
+})();
+
+// ------------ START ------------
+
+Game.start();
+
+
+// --------------------------------------------
+// --------------------------------------------
+//                RESOURCES
+// --------------------------------------------
+// --------------------------------------------
 
 function createPlayer (num, name, symbol) {
     
@@ -57,96 +136,17 @@ function createPlayer (num, name, symbol) {
     return {getNum, getName, getSymbol, getScore, incScore};
 }
 
-// --------------------------------------------
-//                   BOARD
-// --------------------------------------------
-
-const Board_DOM = (function () {
-
-    const blocksNodeList = document.querySelectorAll(".board-block-div");
-
-    const getBlock = (index) => {
-        if (index < 0 || index >= blocksNodeList.length) return null;
-        return blocksNodeList[index];
-    }
-
-    const blockEventListener = (event) => {
-        let index = HelperDOM.findNodeIndex(event.target, blocksNodeList);
-        Game.playTurn(index);
-    }
-
-    // PUBLIC
-
-    const addSignToBlock = (sign, index) => {
-        let block = getBlock(index);
-        if (block != null) {
-            block.textContent = sign;
-            block.classList.remove('block-allow-hover'); 
-        }
-    }
-
-    const addBlockListener = () => {
-        for (let i=0; i < blocksNodeList.length; i++) {
-            blocksNodeList[i].addEventListener("click", blockEventListener);
-        }
-    }
-
-    const resetBlocks = () => {
-        for (let i=0; i< blocksNodeList.length; i++) {
-            addSignToBlock(emptyString, i);
-            getBlock(i).classList.add('block-allow-hover'); 
-            blocksNodeList[i].removeEventListener("click", blockEventListener);
-        }
-    }
-
-    return {addSignToBlock, addBlockListener, resetBlocks};
+const Players_DOM = (function () {
     
-  })();
+    const playerScores = document.querySelectorAll(".player-score");
 
-  const Board_Helper = (function () {
-
-    const indexToRowCol = (index) => {
-        const row = Math.floor(index / lineSize);
-        const col = index % lineSize;
-      
-        return { row, col };
-    };
-    const rowColToIndex = (row, col) => {
-        return (row*lineSize + col);
-    };
-
-    const checkRowForWin = (row, blocks, sign) => {
-        for (let i=0; i < lineSize; i++) {
-            if (blocks[row*lineSize + i] != sign) return false;
-        }
-        return true;
+    const incScore = (playerNum, score) => {
+        let index = playerNum -1;
+        if (index < 0 || index > playerScores.length) return;
+        playerScores[index].textContent = "Score: "+score.toString(10);
     }
 
-    const checkColForWin = (col, blocks, sign) => {
-        for (let i=0; i < lineSize; i++) {
-            if (blocks[i*lineSize + col] != sign) return false;
-        }
-        return true;
-    }
-
-    const checkForDiagWin = (blocks, sign) => {
-        if (blocks[0] == sign && blocks[4] == sign && blocks[8] == sign) return true;
-        if (blocks[2] == sign && blocks[4] == sign &&  blocks[6] == sign) return true;
-        return false;
-    }
-
-    const checkWin = (blocks, sign, mostRecentAddIndex) => {
-        let {row, col} = Board_Helper.indexToRowCol(mostRecentAddIndex);
-        if (   checkRowForWin(row, blocks, sign) 
-            || checkColForWin(col, blocks, sign) 
-            || checkForDiagWin(blocks, sign)) 
-        {
-            return true;
-        }
-        return false;
-    }
-
-    return {indexToRowCol, rowColToIndex, checkWin};
+    return {incScore };
 })();
 
 const Board = (function () {
@@ -193,106 +193,117 @@ const Board = (function () {
         Board_DOM.addBlockListener();
     }
 
-
-
     return {checkWin, checkFull, addSignToBlock, reset, allowPlaying};
 })();
 
+const Board_DOM = (function () {
+
+    const blocksNodeList = document.querySelectorAll(".board-block-div");
+
+    const getBlock = (index) => {
+        if (index < 0 || index >= blocksNodeList.length) return null;
+        return blocksNodeList[index];
+    }
+
+    const blockEventListener = (event) => {
+        let index = HelperDOM.findNodeIndex(event.target, blocksNodeList);
+        Game.playTurn(index);
+    }
+
+    // PUBLIC
+
+    const addSignToBlock = (sign, index) => {
+        let block = getBlock(index);
+        if (block != null) {
+            block.textContent = sign;
+            block.classList.remove('block-allow-hover'); 
+        }
+    }
+
+    const addBlockListener = () => {
+        for (let i=0; i < blocksNodeList.length; i++) {
+            blocksNodeList[i].addEventListener("click", blockEventListener);
+        }
+    }
+
+    const resetBlocks = () => {
+        for (let i=0; i< blocksNodeList.length; i++) {
+            addSignToBlock(emptyString, i);
+            getBlock(i).classList.add('block-allow-hover'); 
+            blocksNodeList[i].removeEventListener("click", blockEventListener);
+        }
+    }
+
+    return {addSignToBlock, addBlockListener, resetBlocks};
+    
+})();
 
 // --------------------------------------------
-//                   GAME
+// --------------------------------------------
+//                 HELPER
+// --------------------------------------------
 // --------------------------------------------
 
-const Game_DOM = (function () {
-    
-    const statusInfo = document.querySelector("#status-info");
-    const startBtn = document.querySelector("#start-btn");
-
-    const hideStartBtn = () => {
-        startBtn.style.display = 'none';
-        startBtn.textContent = "Play Again";
-    }
-
-    const showStartBtn = () => {
-        startBtn.style.display = 'block';
-    }
-
-    const showStatus = () => {
-        let gameState = Game.getGameState();
-        if (gameState == -1 || gameState == -2) {
-            return;
+const HelperDOM = (function () {
+    const findNodeIndex = (node, nodeList) => {
+        for (let i = 0; i < nodeList.length; i++) {
+            if (nodeList[i] === node) {
+                return i;
+            }
         }
-        if (gameState == 0) {
-            statusInfo.textContent = "Tie"; 
-        } else {
-            statusInfo.textContent = "Player"+gameState+" Won!"; 
-        }
-    }
-
-    return { hideStartBtn, showStartBtn, showStatus };
-  })();
-
-const Game = (function () {
-    
-    const player1 = createPlayer(1, "player1", "X");
-    const player2 = createPlayer(2, "player2", "O");
-    let currPlayer = player1;
-    let gameEnded = false;
-    let gameState = -1;
-    // DOM
-    const statusInfo = document.querySelector("#status-info");
-    const startBtn = document.querySelector("#start-btn");
-
-    const reset = () => {
-        currPlayer = player1;
-        gameEnded = false;
-        Board.reset();
-        gameState = -1;
-        statusInfo.textContent = "";
-        console.log("Reset Game");
-    }
-
-    const playTurn_aux = (index) => {
-        if (gameEnded) {
-            console.log("Game has already ended.");
-            return currPlayer.getNum();
-        }
-        if (!Board.addSignToBlock(currPlayer, index)) {
-            return -2;
-        }
-
-        if (Board.checkWin()) {
-            currPlayer.incScore();
-            return currPlayer.getNum();
-        }
-        if (Board.checkFull()) {
-            return 0;
-        }
-
-        currPlayer == player1 ? currPlayer = player2 : currPlayer = player1;
         return -1;
     }
 
-    const playTurn = (index) => {
-        gameState = playTurn_aux(index);
-        if (gameState >=0 ) {
-            gameEnded = true;
-            Game_DOM.showStartBtn();
-            Game_DOM.showStatus();
+    return { findNodeIndex };
+})();
+
+const Board_Helper = (function () {
+
+    const indexToRowCol = (index) => {
+        const row = Math.floor(index / lineSize);
+        const col = index % lineSize;
+      
+        return { row, col };
+    };
+    const rowColToIndex = (row, col) => {
+        return (row*lineSize + col);
+    };
+
+    const checkRowForWin = (row, blocks, sign) => {
+        for (let i=0; i < lineSize; i++) {
+            if (blocks[row*lineSize + i] != sign) return false;
         }
+        return true;
     }
 
-    const getGameState = () => gameState;
-
-    const start = () => {
-        startBtn.addEventListener("click", () => {
-            reset();
-            Game_DOM.hideStartBtn();
-            Board.allowPlaying();
-        });
+    const checkColForWin = (col, blocks, sign) => {
+        for (let i=0; i < lineSize; i++) {
+            if (blocks[i*lineSize + col] != sign) return false;
+        }
+        return true;
     }
 
-    return {playTurn, getGameState, start};
-  })();
+    const checkForDiagWin = (blocks, sign) => {
+        if (blocks[0] == sign && blocks[4] == sign && blocks[8] == sign) return true;
+        if (blocks[2] == sign && blocks[4] == sign &&  blocks[6] == sign) return true;
+        return false;
+    }
 
-  Game.start();
+    const checkWin = (blocks, sign, mostRecentAddIndex) => {
+        let {row, col} = Board_Helper.indexToRowCol(mostRecentAddIndex);
+        if (   checkRowForWin(row, blocks, sign) 
+            || checkColForWin(col, blocks, sign) 
+            || checkForDiagWin(blocks, sign)) 
+        {
+            return true;
+        }
+        return false;
+    }
+
+    return {indexToRowCol, rowColToIndex, checkWin};
+})();
+
+
+
+
+
